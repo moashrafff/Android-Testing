d8e3dfc60859baf8fdc7b7054d3dd4b727721d5c
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-05-29 20:38:25 +0300

    add expected and ignore

--- app/src/main/java/com/moashraf/testingcourse/LeadsController.kt
+++ app/src/main/java/com/moashraf/testingcourse/LeadsController.kt
@@ -5,16 +5,23 @@ class LeadsController {
     val leadsList = mutableListOf<Lead>()
 
     fun addLeads(lead: Lead) {
+        if (isPhoneNumberLetters(lead.phoneNumber)) throw IllegalArgumentException("phone number contains letters")
+
         if (isValidPhoneNumber(lead.phoneNumber) && isUniqueLead(lead)) {
-        leadsList.add(lead)
+            leadsList.add(lead)
         }
     }
 
-    fun getLeads() : List<Lead> = leadsList
+    fun getLeads(): List<Lead> = leadsList
 
-    private fun isValidFirstName(firstName: String) : Boolean = firstName.length >= 3
+    private fun isValidFirstName(firstName: String): Boolean = firstName.length >= 3
 
-    private fun isValidPhoneNumber (phoneNumber : String) : Boolean = phoneNumber.length == 11 && phoneNumber.all { char -> char.isDigit() }
+    private fun isValidPhoneNumber(phoneNumber: String): Boolean =
+        phoneNumber.length == 11 && phoneNumber.all { char -> char.isDigit() }
 
-    private fun isUniqueLead (lead: Lead) : Boolean = leadsList.none { it.firstName == lead.firstName && it.lastName == lead.lastName && it.phoneNumber == lead.phoneNumber }
+    private fun isPhoneNumberLetters(phoneNumber: String): Boolean =
+        phoneNumber.all { char -> char.isLetter() }
+
+    private fun isUniqueLead(lead: Lead): Boolean =
+        leadsList.none { it.firstName == lead.firstName && it.lastName == lead.lastName && it.phoneNumber == lead.phoneNumber }
 }
--- app/src/test/java/com/moashraf/testingcourse/leads/LeadsControllerTest.kt
+++ app/src/test/java/com/moashraf/testingcourse/leads/LeadsControllerTest.kt
@@ -6,11 +6,12 @@ import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 
 class LeadsControllerTest {
 
-    var controller : LeadsController? = null
+    var controller: LeadsController? = null
 
     @Before
     fun setUp() {
@@ -18,8 +19,8 @@ class LeadsControllerTest {
     }
 
     @Test
-    fun `Given valid lead when addLeads then return one item list` (){
-        val lead = Lead(firstName = "Nadine", lastName = "Islam", phoneNumber =  "01026656520")
+    fun `Given valid lead when addLeads then return one item list`() {
+        val lead = Lead(firstName = "Nadine", lastName = "Islam", phoneNumber = "01026656520")
 
         controller?.addLeads(lead)
         val leads = controller?.getLeads()
@@ -27,7 +28,7 @@ class LeadsControllerTest {
     }
 
     @Test
-    fun `Given lead with two characters in first name when calling add lead then return leads is empty` () {
+    fun `Given lead with two characters in first name when calling add lead then return leads is empty`() {
         val lead = Lead(firstName = "mo", lastName = "ashraf", phoneNumber = "1234")
         controller?.addLeads(lead)
         val leads = controller?.getLeads()
@@ -35,7 +36,7 @@ class LeadsControllerTest {
     }
 
     @Test
-    fun `Given lead with two characters in last name when calling add lead then return leads is empty` () {
+    fun `Given lead with two characters in last name when calling add lead then return leads is empty`() {
         val lead = Lead(firstName = "Mohamed", lastName = "as", phoneNumber = "1234")
         controller?.addLeads(lead)
         val leads = controller?.getLeads()
@@ -43,21 +44,28 @@ class LeadsControllerTest {
     }
 
     @Test
-    fun `Given lead with invalid phone number when calling add lead then return leads is empty` () {
+    fun `Given lead with invalid phone number when calling add lead then return leads is empty`() {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "1234")
         controller?.addLeads(lead)
         assertTrue(controller?.getLeads()?.isEmpty() == true)
     }
 
     @Test
-    fun `Given lead with invalid phone number with chars not digits when calling add lead then return leads is empty` () {
+    fun `Given lead with invalid phone number with chars not digits when calling add lead then return leads is empty`() {
+        val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "010266520")
+        controller?.addLeads(lead)
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `Given lead with invalid phone number with chars when calling add lead then return leads is empty and exception is thrown`() {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "aaaaaaaaaaa")
         controller?.addLeads(lead)
         assertTrue(controller?.getLeads()?.isEmpty() == true)
     }
 
     @Test
-    fun `Given lead with duplicated info when calling add lead then return leads with one item` () {
+    fun `Given lead with duplicated info when calling add lead then return leads with one item`() {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "01026656520")
         val lead2 = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "01026656520")
         controller?.addLeads(lead)
@@ -65,8 +73,19 @@ class LeadsControllerTest {
         assertEquals(1, controller?.getLeads()?.size)
     }
 
+    // try to ignore test cases
+    @Ignore("This test is not ready yet")
+    @Test
+    fun `Given lead with unique info when calling add lead then return leads with one item`() {
+        val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "01026656520")
+        val lead2 = Lead(firstName = "Mohamed", lastName = "mohamed", phoneNumber = "01026656520")
+        controller?.addLeads(lead)
+        controller?.addLeads(lead2)
+        assertEquals(1, controller?.getLeads()?.size)
+    }
+
     @After
-    fun finish(){
+    fun finish() {
         controller = null
     }
 
