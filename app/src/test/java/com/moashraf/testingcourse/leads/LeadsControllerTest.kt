28ce1c0a0024717c14f262c2e3ea5eeadc83b937
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-05-29 19:31:54 +0300

    add before and after

--- app/src/test/java/com/moashraf/testingcourse/leads/LeadsControllerTest.kt
+++ app/src/test/java/com/moashraf/testingcourse/leads/LeadsControllerTest.kt
@@ -4,60 +4,70 @@ import com.moashraf.testingcourse.Lead
 import com.moashraf.testingcourse.LeadsController
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class LeadsControllerTest {
 
+    var controller : LeadsController? = null
+
+    @Before
+    fun setUp() {
+        controller = LeadsController()
+    }
+
     @Test
     fun `Given valid lead when addLeads then return one item list` (){
         val lead = Lead(firstName = "Nadine", lastName = "Islam", phoneNumber =  "01026656520")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        val leads = controller.getLeads()
-        assertEquals(1, leads.size)
+
+        controller?.addLeads(lead)
+        val leads = controller?.getLeads()
+        assertEquals(1, leads?.size)
     }
 
     @Test
     fun `Given lead with two characters in first name when calling add lead then return leads is empty` () {
         val lead = Lead(firstName = "mo", lastName = "ashraf", phoneNumber = "1234")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        val leads = controller.getLeads()
-        assertEquals(0, leads.size)
+        controller?.addLeads(lead)
+        val leads = controller?.getLeads()
+        assertEquals(0, leads?.size)
     }
 
     @Test
     fun `Given lead with two characters in last name when calling add lead then return leads is empty` () {
         val lead = Lead(firstName = "Mohamed", lastName = "as", phoneNumber = "1234")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        val leads = controller.getLeads()
-        assertEquals(0, leads.size)
+        controller?.addLeads(lead)
+        val leads = controller?.getLeads()
+        assertEquals(0, leads?.size)
     }
 
     @Test
     fun `Given lead with invalid phone number when calling add lead then return leads is empty` () {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "1234")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        assertTrue(controller.getLeads().isEmpty())
+        controller?.addLeads(lead)
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
     }
 
     @Test
     fun `Given lead with invalid phone number with chars not digits when calling add lead then return leads is empty` () {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "aaaaaaaaaaa")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        assertTrue(controller.getLeads().isEmpty())
+        controller?.addLeads(lead)
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
     }
 
     @Test
     fun `Given lead with duplicated info when calling add lead then return leads with one item` () {
         val lead = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "01026656520")
         val lead2 = Lead(firstName = "Mohamed", lastName = "ashraf", phoneNumber = "01026656520")
-        val controller = LeadsController()
-        controller.addLeads(lead)
-        controller.addLeads(lead2)
-        assertEquals(1, controller.getLeads().size)
+        controller?.addLeads(lead)
+        controller?.addLeads(lead2)
+        assertEquals(1, controller?.getLeads()?.size)
     }
+
+    @After
+    fun finish(){
+        controller = null
+    }
+
 }
