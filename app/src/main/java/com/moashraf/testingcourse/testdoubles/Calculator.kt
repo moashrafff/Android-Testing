05b91bb0dc94b0eafe3dcea4d766a4f2bdef7d97
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-05-31 19:22:39 +0300

    test arguments capturing and matching

--- app/src/main/java/com/moashraf/testingcourse/testdoubles/Calculator.kt
+++ app/src/main/java/com/moashraf/testingcourse/testdoubles/Calculator.kt
@@ -6,3 +6,11 @@ data class Dependency2(val value: Int)
 class Calculator(val dependency1: Dependency1, val dependency2: Dependency2) {
     fun calculate() = dependency1.value + dependency2.value
 }
+
+class MathService {
+    fun calculate(num1 : Int, num2 : Int) = num1 + num2
+}
+
+class CalculatorV2(val mathService: MathService) {
+    fun add(num1: Int) = mathService.calculate(num1, 10)
+}
--- app/src/test/java/com/moashraf/testingcourse/testdoubles/CalculatorTest.kt
+++ app/src/test/java/com/moashraf/testingcourse/testdoubles/CalculatorTest.kt
@@ -2,7 +2,9 @@ package com.moashraf.testingcourse.testdoubles
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.spyk
+import io.mockk.verify
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -11,10 +13,10 @@ class CalculatorTest {
     // Test Strict vs Relaxed Testing
     @Test
     fun `Given two numbers when add then return sum`() {
-        val dependency1 : Dependency1= mockk(relaxed = true)
-        val dependency2 : Dependency2= mockk()
+        val dependency1: Dependency1 = mockk(relaxed = true)
+        val dependency2: Dependency2 = mockk()
         every { dependency2.value } returns 3
-        val calculator = Calculator(dependency1 = dependency1 , dependency2 = dependency2)
+        val calculator = Calculator(dependency1 = dependency1, dependency2 = dependency2)
         val result = calculator.calculate()
         assertEquals(3, result)
     }
@@ -22,14 +24,45 @@ class CalculatorTest {
     // Test Spy can stub too!
     @Test
     fun `Given two numbers Spy when add then return sum`() {
-        val dependency1 : Dependency1= mockk()
+        val dependency1: Dependency1 = mockk()
         val dependency2 = Dependency2(4)
         val dependency2Spy = spyk(dependency2)
         every { dependency1.value } returns 3
         every { dependency2Spy.value } returns 7
-        val calculator = Calculator(dependency1 = dependency1 , dependency2 = dependency2Spy)
+        val calculator = Calculator(dependency1 = dependency1, dependency2 = dependency2Spy)
         val result = calculator.calculate()
         assertEquals(10, result)
+    }
+
+    // Test Argument Capturing and Matching using slot
+    @Test
+    fun `Given two numbers args catching when add then return sum`() {
+        val mathService: MathService = mockk()
+        val calculator = CalculatorV2(mathService)
+        val slot = slot<Int>()
+        every { mathService.calculate(capture(slot), any()) } answers { arg<Int>(0) + arg<Int>(1) }
+        val result = calculator.add(2)
+
+        assertEquals(12, result)
+
+        assertEquals(2, slot.captured)
+    }
+
+    // Test Argument Capturing and Matching using with args
+    @Test
+    fun `Given two numbers args catching with args when add then return sum`() {
+        val mathService: MathService = mockk()
+        val calculator = CalculatorV2(mathService)
+        every { mathService.calculate(any(), any()) } answers { arg<Int>(0) + arg<Int>(1) }
+        val result = calculator.add(2)
+
+        assertEquals(12, result)
+
+        verify {
+            mathService.calculate(withArg {
+                assertEquals(2, it)
+            }, any())
+        }
     }
 
 }
