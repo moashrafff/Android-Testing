a197f57acc57bcc0d84655d74ffb243d1aa16fc8
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-05-31 18:08:14 +0300

    test Spy

--- app/src/main/java/com/moashraf/testingcourse/testdoubles/PremiumUserManager.kt
+++ app/src/main/java/com/moashraf/testingcourse/testdoubles/PremiumUserManager.kt
@@ -4,6 +4,10 @@ interface UserService {
     fun getUsersCount(): Int
 }
 
+class UserServiceImpl : UserService {
+    override fun getUsersCount(): Int = 20
+}
+
 class PremiumUserManager(val userService: UserService) {
     fun getUsersCount() = userService.getUsersCount()
 }
--- app/src/test/java/com/moashraf/testingcourse/testdoubles/PremiumUserManagerTest.kt
+++ app/src/test/java/com/moashraf/testingcourse/testdoubles/PremiumUserManagerTest.kt
@@ -2,6 +2,7 @@ package com.moashraf.testingcourse.testdoubles
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
 import io.mockk.verify
 import junit.framework.Assert.assertEquals
 import org.junit.Test
@@ -18,6 +19,17 @@ class PremiumUserManagerTest {
         val result = premiumUserManager.getUsersCount()
         assertEquals(100,result)
         verify(atLeast = 1) { userService.getUsersCount() }
+    }
+
+    // Test Spy
+    @Test
+    fun `Given premium user manager spy when get users count then return 20`() {
+        val userService : UserService = UserServiceImpl()
+        val userServiceSpy = spyk(userService)
+        val premiumUserManager = PremiumUserManager(userService = userServiceSpy)
+        val result = premiumUserManager.getUsersCount()
+        assertEquals(20,result)
+        verify(atLeast = 1) { userServiceSpy.getUsersCount() }
     }
 
 }
