e85fc547d7bb57237d4828fa9ec556ee814fc94f
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-05-31 18:21:19 +0300

    test Spy can stub too!

--- app/src/test/java/com/moashraf/testingcourse/testdoubles/CalculatorTest.kt
+++ app/src/test/java/com/moashraf/testingcourse/testdoubles/CalculatorTest.kt
@@ -2,6 +2,7 @@ package com.moashraf.testingcourse.testdoubles
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -16,6 +17,19 @@ class CalculatorTest {
         val calculator = Calculator(dependency1 = dependency1 , dependency2 = dependency2)
         val result = calculator.calculate()
         assertEquals(3, result)
+    }
+
+    // Test Spy can stub too!
+    @Test
+    fun `Given two numbers Spy when add then return sum`() {
+        val dependency1 : Dependency1= mockk()
+        val dependency2 = Dependency2(4)
+        val dependency2Spy = spyk(dependency2)
+        every { dependency1.value } returns 3
+        every { dependency2Spy.value } returns 7
+        val calculator = Calculator(dependency1 = dependency1 , dependency2 = dependency2Spy)
+        val result = calculator.calculate()
+        assertEquals(10, result)
     }
 
 }
