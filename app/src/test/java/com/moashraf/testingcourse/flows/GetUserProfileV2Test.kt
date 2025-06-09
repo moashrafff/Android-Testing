8ba79342e5fbe95af0a92a1dc240e06c5fd51044
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-06-09 16:15:46 +0300

    testing real profile use case with flows with retry case

--- app/src/test/java/com/moashraf/testingcourse/flows/GetUserProfileV2Test.kt
+++ app/src/test/java/com/moashraf/testingcourse/flows/GetUserProfileV2Test.kt
@@ -6,13 +6,17 @@ import com.moashraf.testingcourse.coroutines.UserRepository
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeFalse
 import org.amshove.kluent.shouldBeInstanceOf
 import org.amshove.kluent.shouldBeTrue
 import org.junit.Test
+import java.io.IOException
 import kotlin.coroutines.ContinuationInterceptor
 
 class GetUserProfileV2Test {
@@ -49,23 +53,75 @@ class GetUserProfileV2Test {
         }
 
     @Test
-    fun `Given get profile useCase when calling getProfileDataAsync then return Error`() =
+    fun `Given get profile useCase when calling getProfileDataAsync then return Error`() = runTest {
+        val mockRepo: UserRepository = mockk()
+        val useCase = GetUserProfileV2(
+            mockRepo, this.coroutineContext[ContinuationInterceptor] as CoroutineDispatcher
+        )
+
+        coEvery { mockRepo.getName() } coAnswers { delay(1000); throw IllegalStateException() }
+
+        val flow = useCase.getProfileDataAsync()
+
+        flow.collect {
+            it.isSuccess.shouldBeFalse()
+            it.onFailure {
+                it.shouldBeInstanceOf<IllegalStateException>()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given get profile useCase when calling getProfileDataAsync then retry with return success`() =
         runTest {
             val mockRepo: UserRepository = mockk()
             val useCase = GetUserProfileV2(
                 mockRepo, this.coroutineContext[ContinuationInterceptor] as CoroutineDispatcher
             )
 
-            coEvery { mockRepo.getName() } coAnswers { delay(1000); throw IllegalStateException() }
+            var throwError = true
+            val expectedProfile = Profile(
+                name = "Nadine",
+                friends = listOf(Friend(id = "1", name = "Sara"), Friend(id = "2", name = "Menna")),
+                rate = 5f
+            )
+
+            coEvery { mockRepo.getName() } coAnswers {
+                if (throwError) {
+                    throw IOException()
+                } else {
+                    expectedProfile.name
+                }
+            }
+
+            coEvery { mockRepo.getRate() } coAnswers {
+                if (throwError) {
+                    throw IOException()
+                } else {
+                    expectedProfile.rate
+                }
+            }
+
+            coEvery { mockRepo.getFriends() } coAnswers {
+                if (throwError) {
+                    throw IOException()
+                } else {
+                    expectedProfile.friends
+                }
+            }
 
             val flow = useCase.getProfileDataAsync()
 
-            flow.collect {
-                it.isSuccess.shouldBeFalse()
-                it.onFailure {
-                    it.shouldBeInstanceOf<IllegalStateException>()
+            launch {
+                flow.collect {
+                    it.isSuccess.shouldBeTrue()
                 }
             }
+
+            advanceTimeBy(1000)
+            throwError = false
+            advanceTimeBy(1000)
         }
 
 }
